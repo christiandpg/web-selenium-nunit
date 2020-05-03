09e3ab0ffcdd27462f721bcd0ced75e4c2bc7b33
Christian Goncalves <christian.goncalves@outlook.com>
2020-05-03 20:43:13 -0300

    Initial commit

---
+++ WebSeleniumNunit/WebSeleniumNunit/Tests/AmazonTests.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebSeleniumNunit.Tests
+{
+    class AmazonTests
+    {
+        IWebDriver driver;
+
+        [SetUp]
+        public void startBrowser()
+        {
+            driver = new ChromeDriver("C:\\Users\\chris\\Downloads\\chromedriver_win32(1)\\chromedriver.exe");
+        }
+
+        [Test]
+        public void test()
+        {
+            driver.Url = "http://www.google.co.in";
+        }
+
+        [TearDown]
+        public void closeBrowser()
+        {
+            driver.Close();
+        }
+    }
+}
